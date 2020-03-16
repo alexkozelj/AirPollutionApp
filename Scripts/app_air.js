01057de42dd376423b8da1282349d3445b91c587
aleksandarkozelj@gmail.com <aleksandarkozelj@gmail.com>
2020-03-16 10:34:02 +0100

    set construction of the app

--- Scripts/app_air.js
+++ Scripts/app_air.js
@@ -11,7 +11,6 @@ console.log(weatherLocation.country);
 const weather = new Weather(weatherLocation.city, weatherLocation.state, weatherLocation.country);
 // Init UI object
 const ui = new UI();
-
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather());
 
@@ -20,40 +19,113 @@ document.getElementById('w-change-btn').addEventListener('click', (e) => {
    let city = document.getElementById('city').value;
    let state = document.getElementById('state').value;
    let country = document.getElementById('country').value;
-
-
-
-   if (city.length === 0) {
+   
+   
+   
+   if (city.length === 0 && state.length === 0 && country.length === 0) {
+      country = "Germany";
+      state = "Hessen";
       city = "Frankfurt am Main";
    }
-   if (state.length === 0) {
-      state = "Hessen";
-   }
-   if (country.length === 0) {
-      country = "Germany";
-   }
+   
    console.log(city);
    console.log(state);
    console.log(country);
-
+   
    // Change location
    weather.changeLocation(city, state, country);
-
+   
    // Set location in LS
    storage.setLocationData(city, state, country);
-
+   
    // Get and display weather
    getWeather();
-
-
+   
+   
    // Close modal
    $('#locModal').modal('hide');
 });
 
+// let openClose = 0;
+document.getElementById('pollution-sign').addEventListener('click', (e) => {
+   
+   let aqi = ui.pollutionNumber;
+   const parent = document.getElementById("desc-parent");
+   
+   
+   function togNav() {
+      var nav = document.getElementById("nav");
+      if (nav.style.width == '200px') {
+         nav.style.width = '0';
+         nav.style.opacity = 0;
+      } else {
+         nav.style.width = "200px";
+         nav.style.opacity = 1;
+      }
+   }
+   
+   const good = "Air quality is satisfactory and poses little or no health risk. Ventilating your home is recommended. Recommendations: Enjoy your usual outdoor activities. We recommend opening your windows and ventilating your home to bring in fresh, oxygen-rich air.";
+   
+   // const aqiStr = document.getElementById('w-aqi').innerHTML;
+   // const aqi = parseInt(aqiStr);
+   // if (openClose === 1) {
+      //    openClose = 0;
+      //    const racqu = document.getElementById("description-id");
+      //    // const tataNaRacqu = document.getElementById("parent-div");
+      //    // tataNaRacqu.removeChild(tataNaRacqu.childNodes(7));
+      //    const parent = document.getElementById("desc-parent");
+      //    racqu.remove();
+      // }
+      
+   if(aqi < 50){
+      if(parent.style.height == "") {
+         const description = document.createElement("p");
+         description.setAttribute("class", "description")
+         description.setAttribute("id", "description-id")
+         description.innerHTML = good;
+  
+         // const parent = document.getElementById("desc-parent");
+  
+         parent.appendChild(description);
+      } else {
+         parent.style.height = 0;
+         parent.style.opacity = 0;
+      }
+      // function myFunction() {
+      //    var newItem = document.createElement("LI");
+      //    var textnode = document.createTextNode("Water");
+      //    newItem.appendChild(textnode);
+       
+      //    var list = document.getElementById("myList");
+      //    list.insertBefore(newItem, list.childNodes[0]);
+      //  }
+      // if(openClose === 0) {
+      //    openClose = 1;
+         
+         // const parent = document.getElementById("parent-div");
+         // parent.insertBefore(description, beforeEl);
+      // } 
+   }
+   else if (aqi > 50 && aqi < 101) {
 
+   }
+   else if (aqi > 100 && aqi < 151) {
+
+   }
+   else if (aqi > 150 && aqi < 251) {
+
+   }
+   else if (aqi > 250 && aqi < 301) {
+
+   } else {
+      console.log('something went wrong')
+   }
+
+   e.preventDefault();
+})
 
 function getWeather() {
-   weather.getCountry();
+   // weather.getCountry();
    weather.getWeather()
       .then(results => {
          ui.paint(results);
--- Scripts/ui_air.js
+++ Scripts/ui_air.js
@@ -1,32 +1,73 @@
 class UI {
    constructor() {
       this.location = document.getElementById('w-location');
-      this.desc = document.getElementById('w-desc');
-      this.string = document.getElementById('w-string');
+      this.pollution = document.getElementById('w-aqi');
+      this.pollutionNumber;
+      this.pollutionSign = document.getElementById('pollution-sign');
+      this.desc = document.getElementById('w-temp');
       this.details = document.getElementById('w-details');
       this.icon = document.getElementById('w-icon');
       this.humidity = document.getElementById('w-humidity');
-      this.feelsLike = document.getElementById('w-feels-like');
-      this.dewpoint = document.getElementById('w-dewpoint');
-      this.wind = document.getElementById('w-wind');
+      this.feelsLike = document.getElementById('w-wind-direction');
+      this.dewpoint = document.getElementById('w-pressure');
+      this.wind = document.getElementById('w-wind-speed');
 
    }
 
+   
+
    paint(weather) {
+      
       this.location.textContent = weather.data.city + ", " + weather.data.country;
+      this.pollution.textContent = "US AirQualityIndex: " + weather.data.current.pollution.aqius;
+      this.pollutionNumber = weather.data.current.pollution.aqius;
+      let aqi = weather.data.current.pollution.aqius;
+      // depending of index, button will get certain class
+      if(aqi < 50) {
+         this.pollutionSign.setAttribute("class", "col-md-4 mx-auto text-center mb-4 mt-4 good");
+         this.pollutionSign.innerHTML = "Good";
+         const good = "Air quality is satisfactory and poses little or no health risk. Ventilating your home is recommended. Recommendations: Enjoy your usual outdoor activities. We recommend opening your windows and ventilating your home to bring in fresh, oxygen-rich air.";
+         document.getElementById("description").innerHTML = good;
+      }
+      else if (aqi > 50 && aqi < 101) {
+         this.pollutionSign.setAttribute("class", "btn btn-secondary mb-4 moderate");
+         this.pollutionSign.innerHTML = "Moderate";
+      }
+      else if (aqi > 100 && aqi < 151) {
+         this.pollutionSign.setAttribute("class", "btn btn-secondary mb-4 unhealthy-1");
+         this.pollutionSign.innerHTML = "Unhealthy*";
+      }
+      else if (aqi > 150 && aqi < 251) {
+         this.pollutionSign.setAttribute("class", "btn btn-secondary mb-4 unhealthy-2");
+         this.pollutionSign.innerHTML = "Unhealthy**";
+      }
+      else if (aqi > 250 && aqi < 301) {
+         this.pollutionSign.setAttribute("class", "btn btn-secondary mb-4 hazardous");
+         this.pollutionSign.innerHTML = "Hazardous";
+      }
+      else {
+         alert("Something's wrong, please try latter")
+      }
       this.desc.textContent = weather.data.current.weather.tp + " C°";
-      this.string.textContent = "US AirQualityIndex: " + weather.data.current.pollution.aqius;
-      this.icon.setAttribute('src', "http://openweathermap.org/img/wn/" + weather.data.current.weather.ic + "@2x.png");
+      // if there is a fog, change the N character to D to get the icon from server
+      this.icon.setAttribute('src', "https://airvisual.com/images/" + weather.data.current.weather.ic + ".png");
+      if(weather.data.current.weather.ic === "50n") {
+         this.icon.setAttribute('src', "https://airvisual.com/images/50d.png");
+      } 
+      if (weather.data.current.weather.ic === "04n") {
+         this.icon.setAttribute('src', "https://airvisual.com/images/04d.png");
+      }
       this.humidity.textContent = `Relative Humidity: ${weather.data.current.weather.hu}%`;
       this.dewpoint.textContent = `Pressure: ${weather.data.current.weather.pr}mb`;
       this.feelsLike.textContent = `Wind Direction: ${this.degToCompass(weather.data.current.weather.wd)}`;
       this.wind.textContent = "Wind Speed: " + weather.data.current.weather.ws + "km/h";
    }
 
-   // Wind direction from degrees(API) to Compas cardinal direction
+   // Wind direction from degrees(API) to Compass cardinal direction
    degToCompass(num) {
       var val = Math.floor((num / 22.5) + 0.5);
       var arr = ["North", "North-NorthEast", "North-East", "East-NorthEast", "East", "East-SouthEast", "South-East", "South-SouthEast", "South", "South-SouthWest", "South-West", "West-SouthWest", "West", "West-NorthWest", "North-West", "North-NorthWest"];
       return arr[(val % 16)];
    }
+
 }

a9176a45603690ef7cbd1a714728c687e0a24cbd
aleksandarkozelj@gmail.com <aleksandarkozelj@gmail.com>
2020-04-04 21:51:05 +0200

    set the state input field

--- Scripts/autocomplete.js
+++ Scripts/autocomplete.js
@@ -17,7 +17,7 @@ class Autocomplete {
       // matches of countries from input 
       this.countryMatches;
       // input box for countries
-      this.country;
+      this.countryInput;
 
 
       // STATE //
@@ -26,17 +26,15 @@ class Autocomplete {
       this.states;
       // state from list of suggestions
       this.stateMatches;
-      // this.stateMatch;
       // input field for state
       this.stateInput;
 
       // CITY //
 
-      this.cityMatches;
       // cities
       this.cities;
       // city from list
-      this.cityMatch;
+      this.cityMatches;
       // city input field
       this.cityInput;
    }
@@ -100,7 +98,7 @@ class Autocomplete {
                   match = true;
 
                   // set the country value by clicking on a dropdown list of countries
-                  this.country = country.country;
+                  this.countryInput = country.country;
 
                   // add a completion style to input when selection is done
                   countryInput.setAttribute("class", "form-control is-valid");
@@ -114,9 +112,12 @@ class Autocomplete {
                   // clear search list output
                   this.outputHtmlCountry(this.countryMatches);
 
+                  // set focus to state input field
+                  stateInput.focus();
+
                   // GET STATES for a selected country
                   // Serbia states are not complete in api
-                  if (this.country === "Serbia") {
+                  if (this.countryInput === "Serbia") {
                      this.getSerbiaStates();
                   } else {
                      // get states of selected country
@@ -222,29 +223,32 @@ class Autocomplete {
    selectCountry = (e) => {
       // h5 element
       if (e.target.classList.contains("child")) {
-         this.country = e.target.innerText;
+         this.countryInput = e.target.innerText;
       }
       // parent div of h5 element
       if (e.target.classList.contains("parent")) {
-         this.country = e.target.firstElementChild.innerText;
+         this.countryInput = e.target.firstElementChild.innerText;
       }
 
       // set the country value by clicking on a dropdown list of countries
-      countryInput.value = this.country;
+      countryInput.value = this.countryInput;
 
       // add a completion style to input when selection is done
       countryInput.setAttribute("class", "form-control is-valid");
 
       // show state input
       stateForm.style.display = "block";
+      // set focus to state input field
+      stateInput.focus();
       // reset the search list array
       this.countryMatches = [];
       // clear search list output
       this.outputHtmlCountry(this.countryMatches);
 
       // ////////////////////////////////    STATE    ////////////////////////////////////////
+
       // Serbia states are not complete in api
-      if (this.country === "Serbia") {
+      if (this.countryInput === "Serbia") {
          this.getSerbiaStates();
       } else {
          // get states of selected country
@@ -263,8 +267,6 @@ class Autocomplete {
          <h5 class="child">${match.country}</h5>
          </div>
          `).join('');
-
-
          countryMatchList.innerHTML = html;
       } else {
          countryMatchList.innerHTML = '';
@@ -273,12 +275,16 @@ class Autocomplete {
 
    // Clicking on input will reset to input mode
    clickInputCountry = e => {
-      if (e.target.classList.contains("is-valid")) {
+      if (e.target.classList.contains("is-valid") && e.target.id === "countryInput" || e.target.classList.contains("is-invalid") && e.target.id === "countryInput") {
          stateForm.style.display = "none";
-         cityInput.style.display = "none";
-         countryInput.setAttribute("class", "form-control")
+         cityForm.style.display = "none";
+         countryInput.setAttribute("class", "form-control");
+         stateInput.setAttribute("class", "form-control");
+         cityInput.setAttribute("class", "form-control");
+         stateMatchList.innerHTML = '';
          countryInput.value = '';
-
+         stateInput.value = '';
+         cityInput.value = '';
       }
    }
 
@@ -293,15 +299,13 @@ class Autocomplete {
    // //////////////////////////////////////     STATE      ///////////////////////////////////////////
 
    getStates = async () => {
-      const res = await fetch(`https://api.airvisual.com/v2/states?country=${this.country}&key=${this.apiKey}`);
+      const res = await fetch(`https://api.airvisual.com/v2/states?country=${this.countryInput}&key=${this.apiKey}`);
       this.states = await res.json()
-      // console.log(this.states);
    };
 
    getSerbiaStates = async () => {
       const res = await fetch('https://alexkozelj.github.io/AirPollutionApp/serbia_states.json');
       this.states = await res.json();
-      // console.log(this.states);
    };
 
 
@@ -310,84 +314,206 @@ class Autocomplete {
       this.searchInputText = searchText;
       // Get matches to current text input
       this.stateMatches = this.states.data.filter(state => {
-         if(searchText.length > 0){
+         console.log(state);
+         // match more than 1 letter
+         if (searchText.length > 1) {
             const regex = new RegExp(`${searchText}`, 'gi');
             return state.state.match(regex);
          }
 
       });
 
+      console.log(this.stateMatches);
 
       // Clear when input or matches are empty
       if (searchText.length === 0) {
          this.stateMatches = [];
-         this.stateMatches.innerHTML = '';
+         stateMatchList.innerHTML = '';
       }
 
       this.outputHtmlStates(this.stateMatches);
 
    }
 
+
+
+   keyboardSelectState = (e) => {
+      // let currentListIndex = 0;
+
+      // ENTER KEY
+      if (e.keyCode === 13) {
+         // remove cursor when enter key is pressed
+         stateInput.blur();
+         // assign  
+         this.searchInputText = stateInput.value;
+         // if there are no matches on the list, the input value is always false
+         if (this.stateMatches.length === 0) {
+            stateInput.setAttribute("class", "form-control is-invalid");
+         } else {
+            // if there is a match, assign to true
+            let match = false;
+            // go through a list to find a match with input field
+            this.stateMatches.forEach((state) => {
+               // if there is a match
+               if (this.searchInputText.toLowerCase() === state.state.toLowerCase()) {
+                  match = true;
+
+                  // set the country value by clicking on a dropdown list of countries
+                  this.stateInput = state.state;
+
+                  // add a completion style to input when selection is done
+                  stateInput.setAttribute("class", "form-control is-valid");
+                  // reset a index for a next search
+                  this.childIndex = 0;
+                  // show state input field
+                  cityForm.style.display = "block";
+
+                  // reset the search list array
+                  this.stateMatches = [];
+                  // clear search list output
+                  this.outputHtmlStates(this.stateMatches);
+
+                  // GET CITIES for a selected state
+                  // Serbia states are not complete in api
+                  if (this.stateInput === "Kosovo") {
+                     this.getKosovoCities();
+                  } else {
+                     // get cities of selected country
+                     this.getCities();
+                  }
+               }
+
+               if (match === false) {
+                  stateInput.setAttribute("class", "form-control is-invalid");
+               }
+
+            })
+
+         }
+
+         e.preventDefault();
+      }
+
+
+      // DOWN ARROW KEY
+      if (e.keyCode === 40) {
+
+         // end of a list
+         if (this.childIndex === stateMatchList.children.length - 1) {
+            // stay at the last li
+            this.childIndex = this.stateMatches.length - 1;
+         } else {
+            // need a loop while up arrow key changes child index
+            for (let i = 0; i < this.stateMatches.length; i++) {
+               // match the input name and list item to add style
+               if (stateInput.value.toLowerCase() === this.stateMatches[i].state.toLowerCase()) {
+                  this.childIndex = i + 1;
+               }
+            }
+         }
+         // assign input text to the countries from match list
+         stateInput.value = stateMatchList.children[this.childIndex].children[0].innerText;
+
+         // add style to list item 
+         for (let i = 0; i < this.stateMatches.length; i++) {
+            // match the input name and list item to add style
+            if (this.stateMatches[i].state.toLowerCase() === stateInput.value.toLowerCase()) {
+               // in case when it's not the first list item
+               if (i !== 0) {
+                  // default style to previous list item
+                  stateMatchList.children[i - 1].setAttribute("class", "card card-body parent");
+                  // item that is bin selected
+                  stateMatchList.children[i].setAttribute("class", "card card-body parent selectedCard");
+
+               } else {
+                  // if its the first list item, set default style to the last element and add style to fist
+                  stateMatchList.children[this.stateMatches.length - 1].setAttribute("class", "card card-body parent");
+                  stateMatchList.children[i].setAttribute("class", "card card-body parent selectedCard");
+               }
+            }
+         }
+         e.preventDefault();
+      }
+      // UP ARROW KEY
+      if (e.keyCode === 38) {
+         // stays at the first element
+         if (this.childIndex === 0) {
+            // first list item
+            this.childIndex = 0;
+         } else {
+            // going back
+            this.childIndex -= 1;
+         }
+
+         // assign input text to the countries from match list
+         stateInput.value = stateMatchList.children[this.childIndex].children[0].innerText;
+         // add style to list item 
+         for (let i = 0; i < this.stateMatches.length; i++) {
+            // match the input name and list item to add style
+            if (this.stateMatches[i].state.toLowerCase() === stateInput.value.toLowerCase()) {
+               // in case when it's not the first list item
+               if (i === this.stateMatches.length - 1) {
+                  stateMatchList.children[0].setAttribute("class", "card card-body parent");
+                  stateMatchList.children[i].setAttribute("class", "card card-body parent selectedCard");
+               } else {
+                  // default style to previous list item
+                  stateMatchList.children[i + 1].setAttribute("class", "card card-body parent");
+                  // item that is bin selected
+                  stateMatchList.children[i].setAttribute("class", "card card-body parent selectedCard");
+
+               }
+            }
+         }
+
+         e.preventDefault();
+      }
+
+      // BACKSPACE KEY
+      if (e.keyCode === 8) {
+         // reset
+         this.childIndex = 0;
+      }
+   }
+
+
+
+
+
+
+
+
    selectState = (e) => {
       // h5 element
       if (e.target.classList.contains("child")) {
-         this.states = e.target.innerHTML;
+         this.stateInput = e.target.innerHTML;
       }
       // parent div of h5 element
       if (e.target.classList.contains("parent")) {
-         this.states = e.target.firstElementChild.innerHTML;
+         this.stateInput = e.target.firstElementChild.innerHTML;
       }
 
       // search is set to match the state from the list
-      stateInput.value = this.states;
-      console.log(this.states);
+      stateInput.value = this.stateInput;
+
       // countries from the list are always valid
       stateInput.setAttribute("class", "form-control is-valid");
+
+      // show city input
+      cityForm.style.display = "block";
       // state is selected, no need of displaying a list
       this.stateMatches = [];
-      // clear the matching list
-      this.stateMatches.innerHTML = '';
+      // clear search list output
+      this.outputHtmlStates(this.stateMatches);
 
 
       // ////////////////    CITIES     ////////////////////////
 
-      if (this.states === "Kosovo") {
+      if (this.stateInput === "Kosovo") {
          this.getKosovoCities();
       } else {
          // get states of selected country
          this.getCities();
       }
-
-      // city input div
-      const cityDiv = document.createElement('div');
-      cityDiv.setAttribute("id", "cityForm")
-      // city input
-      const city = `
-      <div class="form-group has-success">
-      <label class="form-control-label" for="city / town"> City / Town</label>
-      <input type="text" id="cityInput" class="form-control" placeholder="Enter city" autocomplete="off"/>
-      </div>
-      <div id="city-match-list"></div>
-      `;
-      cityDiv.innerHTML = city;
-      // append city div to a form
-      form.appendChild(cityDiv);
-
-      // input of a state form
-      const cityInput = document.getElementById("cityInput");
-      // listen for state input and search states
-      cityInput.addEventListener('input', () => autocomplete.searchCities(cityInput.value));
-
-      // list of states of the selected country
-      const cityMatchList = document.getElementById("city-match-list");
-      // Select state from the list
-      cityMatchList.addEventListener('click', autocomplete.selectCity);
-
-      cityInput.addEventListener('click', this.clickInputCity);
-
-      this.cityMatch = cityMatchList;
-      this.cityInput = cityInput;
-
 
       e.preventDefault();
 
@@ -395,17 +521,29 @@ class Autocomplete {
 
    // Show results in HTML STATE
    outputHtmlStates = matches => {
-      if (matches.length > 2) {
+      console.log(matches);
+      if (matches.length > 0) {
          const html = matches.map(match => `
          <div class="card card-body parent">
          <h5 class="child">${match.state}</h5>
          </div>
          `).join('');
-         this.stateMatches.innerHTML = html;
+         stateMatchList.innerHTML = html;
       } else {
          // if no input, clear search list
-         this.stateMatches.innerHTML = '';
-         stateInput.setAttribute("class", "form-control is-invalid");
+         stateMatchList.innerHTML = '';
+      }
+   }
+
+   clickInputState = e => {
+      if (e.target.classList.contains("is-valid") && e.target.id === "stateInput" && e.target.classList.contains("is-valid") && e.target.id === "stateInput") {
+         stateInput.setAttribute("class", "form-control");
+         cityForm.style.display = "none";
+         cityMatchList.innerHTML = '';
+         stateInput.value = '';
+         cityInput.value = '';
+         // this.getStates();
+
       }
    }
 
@@ -416,7 +554,7 @@ class Autocomplete {
    // ////////////////////         CITIES         //////////////////////////////
 
    getCities = async () => {
-      const res = await fetch(`https://api.airvisual.com/v2/cities?state=${this.states}&country=${this.country}&key=${this.apiKey}`);
+      const res = await fetch(`https://api.airvisual.com/v2/cities?state=${this.stateInput}&country=${this.countryInput}&key=${this.apiKey}`);
       this.cities = await res.json()
    };
 
@@ -493,15 +631,7 @@ class Autocomplete {
 
 
 
-   clickInputState = e => {
-      if (e.target.classList.contains("is-valid") && e.target.id === "stateInput") {
-         this.stateInput.setAttribute("class", "form-control");
-         this.stateInput.value = '';
-         this.removeElement("cityForm");
-         this.getCountries();
-         this.getStates();
-      }
-   }
+
 
    clickInputCity = e => {
       if (e.target.classList.contains("is-valid") && e.target.id === "cityInput") {
